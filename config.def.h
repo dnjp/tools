bd0bc30add40992701dcea55aaa90eea30a0cb3d
Daniel Posthuma <dnjp@posteo.org>
2021-06-20 10:11:00 -0400

    switch arch to arm64 and add java and javascript comment handling

--- config.def.h
+++ config.def.h
@@ -1,6 +1,8 @@
 #include "ft.h"
 
 static struct filetype filetypes[] = {
-	{ "// ", NULL, {".go", NULL} },      /* Go */
-	{ "/* ", " */", {".c", ".h", NULL} }, /* C */
+	{ "// ", NULL, {".go", NULL} },              /* Go */
+	{ "// ", NULL, {".java", NULL} },            /* Java */
+	{ "// ", NULL, {".js", ".ts", NULL} },       /* Javascript */
+	{ "/* ", " */", {".c", ".h", NULL} },        /* C */
 };
--- config.mk
+++ config.mk
@@ -3,10 +3,10 @@ VERSION = 0.1
 PREFIX    = /usr/local
 MANPREFIX = ${PREFIX}/share/man
 
-BINDIR    = ${HOME}/bin/amd64
+BINDIR    = ${HOME}/bin/arm64
 
-CPPFLAGS = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_POSIX_C_SOURCE=200809L -DVERSION=\"${VERSION}\" 
+CPPFLAGS = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_POSIX_C_SOURCE=200809L -DVERSION=\"${VERSION}\"
 CFLAGS   = -std=c99 -pedantic -Wall -Wno-deprecated-declarations -Os ${CPPFLAGS}
-LDFLAGS  = 
+LDFLAGS  =
 
 CC = clang
