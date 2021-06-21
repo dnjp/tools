fca493ddd0aaf49295cb2658eca47553fb0d6796
Daniel Posthuma <dnjp@posteo.org>
2021-06-21 18:54:11 -0400

    clear out c project

--- config.def.h
+++
@@ -1,8 +0,0 @@
-#include "ft.h"
-
-static struct filetype filetypes[] = {
-	{ "// ", NULL, {".go", NULL} },              /* Go */
-	{ "// ", NULL, {".java", NULL} },            /* Java */
-	{ "// ", NULL, {".js", ".ts", NULL} },       /* Javascript */
-	{ "/* ", " */", {".c", ".h", NULL} },        /* C */
-};
