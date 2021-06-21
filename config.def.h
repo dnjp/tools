9ffa74d8472a290716523406105c5f80712dea26
Daniel Posthuma <dnjp@posteo.org>
2021-06-21 18:53:19 -0400

    add new rules for other languages

--- config.def.h
+++ config.def.h
@@ -3,6 +3,8 @@
 static struct filetype filetypes[] = {
 	{ "// ", NULL, {".go", NULL}, true, 8 },              /* Go */
 	{ "// ", NULL, {".java", NULL}, false, 2 },           /* Java */
-	{ "// ", NULL, {".js", ".ts", NULL}, false, 2 },       /* Javascript */
+	{ "// ", NULL, {".js", ".ts", NULL}, false, 2 },      /* Javascript */
+	{ "# ", NULL, {".yaml", ".yml", NULL}, false, 2 },    /* Yaml */
+	{ "# ", NULL, {".tf", NULL}, false, 2 },              /* Terraform */
 	{ "/* ", " */", {".c", ".h", NULL}, true, 8 },        /* C */
 };
